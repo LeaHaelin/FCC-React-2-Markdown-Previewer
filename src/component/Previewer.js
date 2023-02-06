5a21b72c464c0865d500fe9236b39dc83d826080
LeaHaelin <lea8haelin@gmail.com>
2023-02-06 14:27:22 +0100

    second commit

--- src/component/Previewer.js
+++ src/component/Previewer.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
-export const Previewer = ({ inputText, setInputText, marked }) => {
-  const markdown = marked(inputText, { break: true });
+export const Previewer = ({ inputText, marked }) => {
+  const markdown = marked(inputText, { breaks: true });
+
 
   return (
     <div className="previewer" >
-      <h2 className="previewer__header  box-header">Previewer</h2>
-      <div className="previewer__text" id='preview' dangerouslySetInnerHTML={{ __html: markdown }} ></div>
+      <h3 className="previewer__header  box-header" >Previewer</h3>
+      <div className="previewer__text" id='preview' dangerouslySetInnerHTML={{ __html: markdown }}  ></div>
     </div>
   )
 }
